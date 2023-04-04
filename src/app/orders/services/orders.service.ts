1a0b397a5046a2b5f1060b68305dafe951a8ce3a
cyran <gs.gouin@gmail.com>
2023-04-04 13:42:23 +0200

    Jour 2 apres midi 1

---
+++ src/app/orders/services/orders.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  collection: any = [
+    {
+      "tjmHt": 1200,
+      "nbJours": 10,
+      "tva": 20,
+      "state": "CANCELLED",
+      "typePresta": "coachingDFGH",
+      "client": "Modis",
+      "comment": "Merci Modis pour les 10k",
+      "id": 1
+    },
+    {
+      "tjmHt": 1200,
+      "nbJours": 1,
+      "tva": 20,
+      "state": "CONFIRMED",
+      "typePresta": "coaching",
+      "client": "M2i",
+      "comment": null,
+      "id": 2
+    }
+  ]
+
+  constructor() {
+  }
+}
